8b52190acf5530718db043038a399a6289e32d2f
wuyi0020 <84546163+wuyi0020@users.noreply.github.com>
2023-08-26 20:26:55 +0800

    fix lossless setting
    fix lossless setting

--- MP4_2_webp.py
+++ MP4_2_webp.py
@@ -1,11 +1,10 @@
 import subprocess
-import ffmpeg
-import sys
 import os
+import sys
 
-def convert_mp4_to_webp(mp4_path, output_path):
+def convert_mp4_to_webp(mp4_path, output_path,ffmpeg_path):
     try:
-        subprocess.run(['./bin/ffmpeg.exe', '-i', mp4_path, '-c:v', 'libwebp', '-an', '-vsync', '0', '-loop', '0', '-f', 'webp', output_path])
+        subprocess.run([ffmpeg_path, '-i', mp4_path, '-c:v', 'libwebp', '-lossless', '1', '-an', '-vsync', '0', '-loop', '0', '-f', 'webp', output_path])
         print("Conversion successful!")
     except Exception as e:
         print("Conversion failed:", e)
@@ -14,13 +13,13 @@ def is_mp4_file(file_path):
     return file_path.lower().endswith('.mp4')
 
 if len(sys.argv) > 1:
-    exe_path = os.path.abspath(sys.argv[0])
-    script_directory = os.path.dirname(exe_path)
+    script_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
+
     for mp4_path in sys.argv[1:]:
         if os.path.isfile(mp4_path) and is_mp4_file(mp4_path):
             mp4_path = os.path.abspath(mp4_path)
-            file_path = os.path.splitext(mp4_path)[0] + ".webp"
-            file_path = os.path.basename(file_path)
-            full_path = os.path.join(script_directory,file_path)
-            print(mp4_path)
-            convert_mp4_to_webp(mp4_path, full_path)
+            mp4_filename = os.path.basename(mp4_path)
+            webp_filename = os.path.splitext(mp4_filename)[0] + ".webp"
+            full_webp_path = os.path.join(script_directory, webp_filename)
+            ffmpeg_path = os.path.join(script_directory,'./bin/ffmpeg.exe')
+            convert_mp4_to_webp(mp4_path, full_webp_path,ffmpeg_path)
